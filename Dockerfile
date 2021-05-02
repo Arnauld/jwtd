93586ab64896138da759b2fda05058f9cc81de95
Arnauld <arnauld.loyer@gmail.com>
2021-05-02 18:55:48 +0200

    feat: auth+sign endpoints

--- Dockerfile
+++ Dockerfile
@@ -1,29 +1,21 @@
-FROM ekidd/rust-musl-builder:stable as builder
-
-WORKDIR /app
-COPY . /app
+# syntax=docker/dockerfile:1
+FROM ekidd/rust-musl-builder:1.51.0 as builder
+WORKDIR /home/rust/src
+COPY . /home/rust/src
 RUN cargo build --release
 
 
-FROM alpine:latest as final
-
+FROM alpine:3.13.5 as final
 WORKDIR /app
-
 EXPOSE 8000
-
 ENV TZ=Etc/UTC \
     APP_USER=appuser
-
 RUN addgroup -S $APP_USER \
     && adduser -S -g $APP_USER $APP_USER
-
 RUN apk update \
     && apk add --no-cache ca-certificates tzdata \
     && rm -rf /var/cache/apk/*
-COPY --from=build /app/target/x86_64-unknown-linux-musl/release/jwtd /app/jwtd
-
+COPY --from=builder /home/rust/src/target/x86_64-unknown-linux-musl/release/jwtd /app/jwtd
 RUN chown -R $APP_USER:$APP_USER /app/jwtd
-
 USER $APP_USER
-
 CMD ["./jwtd"]
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,52 @@
 use warp::Filter;
 use serde::{Serialize, Deserialize};
 use jsonwebtoken::{encode, Header, Algorithm, EncodingKey};
-use jsonwebtoken::errors::Result;
 use chrono::prelude::*;
+use std::env;
+use std::fs;
+use std::result;
+use std::fmt;
+use std::convert::Infallible;
+
+/// A crate private constructor for `Error`.
+pub(crate) fn new_error(kind: ErrorKind) -> Error {
+    Error(Box::new(kind))
+}
+
+/// A type alias for `Result<T, jwtd::Error>`.
+pub type Result<T> = result::Result<T, Error>;
+
+#[derive(Debug)]
+pub struct Error(Box<ErrorKind>);
+
+impl Error {
+    /// Return the specific type of this error.
+    pub fn kind(&self) -> &ErrorKind {
+        &self.0
+    }
+
+    /// Unwrap this error into its underlying type.
+    pub fn into_kind(self) -> ErrorKind {
+        *self.0
+    }
+}
+
+#[non_exhaustive]
+#[derive(Debug)]
+pub enum ErrorKind {
+    TokenError(jsonwebtoken::errors::Error),
+    PrivateKeyReadingError(std::io::Error),
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self.0 {
+            ErrorKind::TokenError(ref err) => write!(f, "Token error: {}", err),
+            ErrorKind::PrivateKeyReadingError(ref err) => write!(f, "PrivateKey reading error: {}", err),
+        }
+    }
+}
+
 
 /// Our claims struct, it needs to derive `Serialize` and/or `Deserialize`
 #[derive(Debug, Serialize, Deserialize)]
@@ -14,33 +58,114 @@ struct Claims {
     iss: String,         // Optional. Issuer
 }
 
-pub fn generate_token(aid: &String) -> Result<String> {
+// The query parameters for list_todos.
+#[derive(Debug, Deserialize)]
+pub struct Auth {
+    pub aid: Option<String>,
+    pub roles: Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct SignOpts {
+    pub generate_iat: Option<bool>,
+}
+
+
+fn private_key() -> Result<String> {
+    let location = env::var("PRIV_KEY_LOCATION")
+                        .expect("Environment variable 'PRIV_KEY_LOCATION' not set; unable to read private key");
+
+    return fs::read_to_string(location)
+                .map_err(|err| new_error(ErrorKind::PrivateKeyReadingError(err)));
+}
+
+pub fn generate_token<T: Serialize>(claims: &T) -> Result<String> {
     let header = Header::new(Algorithm::HS256);
+    return encode(&header, &claims, &EncodingKey::from_secret("secret".as_ref()))
+            .map_err(|err| new_error(ErrorKind::TokenError(err)));
+}
+
+fn json_to_Auth() -> impl Filter<Extract = (Auth,), Error = warp::Rejection> + Clone {
+    // When accepting a body, we want a JSON body
+    // (and to reject huge payloads)...
+    warp::body::content_length_limit(1024 * 16).and(warp::body::json())
+}
+
+pub async fn auth_token(opts: Auth) -> result::Result<impl warp::Reply, Infallible> {
+    log::debug!("auth_token: {:?}", opts);
+
+    let aid = match opts.aid {
+                    Some(s) => s,
+                    None => "AGENT:007".to_string(),
+                };
     let issued_at = Utc::now().timestamp();
     let expiration = Utc::now()
         .checked_add_signed(chrono::Duration::seconds(60))
         .expect("valid timestamp")
         .timestamp();
-
     let claims = Claims {
         sub: aid.to_owned(),
         aud: "".to_string(),
         iat: issued_at as usize,
         exp: expiration as usize,
         iss: "jwtd".to_string(),
-
     };
-    return encode(&header, &claims, &EncodingKey::from_secret("secret".as_ref()));
+    let token = generate_token(&claims)
+                    .expect("Failed to generate token...");
+    Ok(warp::reply::json(&token))
 }
 
+pub async fn sign_claims(body: serde_json::Value, signOpts: SignOpts) -> result::Result<impl warp::Reply, Infallible> {
+    log::debug!("sign_claims: {:?} // {:?}", body, signOpts);
+    let token = generate_token(&body)
+                    .expect("Failed to generate token...");
+    Ok(warp::reply::json(&token))
+}
 
 #[tokio::main]
 async fn main() {
+    if env::var_os("RUST_LOG").is_none() {
+        // Set `RUST_LOG=jwtd=debug` to see debug logs,
+        // info - only shows access logs.
+        env::set_var("RUST_LOG", "jwtd=debug");
+    }
+    pretty_env_logger::init();
+
     // GET /hello/warp => 200 OK with body "Hello, warp!"
     let hello = warp::path!("hello" / String)
-        .map(|name| format!("Hello, {}!", name));
+                    .and(warp::get())
+                    .map(|name| format!("Hello, {}!", name));
+    let auth  = warp::path!("auth")
+                    .and(warp::post())
+                    .and(json_to_Auth())
+                    .and_then(auth_token);
 
-    warp::serve(hello)
-        .run(([127, 0, 0, 1], 3030))
+    let sign = warp::path!("sign")
+                    .and(warp::post())
+                    .and(warp::body::content_length_limit(1024 * 32))
+                    .and(warp::body::json())
+                    .and(warp::query::<SignOpts>())
+                    .and_then(sign_claims);
+
+    let port = env::var("PORT")
+                    .map(|a| match a.parse() {
+                                        Ok(n) => n,
+                                        err => {
+                                            eprintln!("error: port not an integer {:?}, fallback on default", err);
+                                            8080
+                                        }
+                                    })
+                    .unwrap_or_else(|_err| {
+                        eprintln!("Port not provided, fallback on default");
+                        8080
+                    });
+
+    let routes = hello
+                    .or(auth)
+                    .or(sign);
+
+    println!("Server starting on port {}", port);
+    warp::serve(routes)
+        .run(([127, 0, 0, 1], port))
         .await;
 }
